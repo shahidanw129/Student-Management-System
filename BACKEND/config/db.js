21ebcb2a7ee2dfbcc136db8551cd4996b7d4a2a6
shahid demo <shahidanwar9978@gmail.com>
2026-04-06 14:37:33 +0530

    Fixed MongoDB Atlas connection

--- BACKEND/config/db.js
+++ BACKEND/config/db.js
@@ -1,8 +1,21 @@
+// const mongoose = require("mongoose");
+
+// const connectDB = async () => {
+//   try {
+//     await mongoose.connect("mongodb://127.0.0.1:27017/studentDB");
+//     console.log("MongoDB Connected ✅");
+//   } catch (error) {
+//     console.log(error.message);
+//     process.exit(1);
+//   }
+// };
+
+// module.exports = connectDB;
 const mongoose = require("mongoose");
 
 const connectDB = async () => {
   try {
-    await mongoose.connect("mongodb://127.0.0.1:27017/studentDB");
+    await mongoose.connect(process.env.MONGO_URI);
     console.log("MongoDB Connected ✅");
   } catch (error) {
     console.log(error.message);
