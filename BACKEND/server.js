284ecb4c9d07962a74fb7d6e4a174f34ef6230ad
shahid demo <shahidanwar9978@gmail.com>
2026-04-06 13:23:58 +0530

    Fixed PORT issue

--- BACKEND/server.js
+++ BACKEND/server.js
@@ -1,3 +1,23 @@
+// const express = require("express");
+// const cors = require("cors");
+// const connectDB = require("./config/db");
+
+// const app = express();
+
+// // middleware
+// app.use(express.json());
+// app.use(cors());
+
+// // DB connect
+// connectDB();
+
+// // routes
+// app.use("/api", require("./routes/studentRoutes"));
+
+// // server start
+// app.listen(5000, () => {
+//   console.log("Server running on port 5000 🚀");
+// });
 const express = require("express");
 const cors = require("cors");
 const connectDB = require("./config/db");
@@ -15,6 +35,8 @@ connectDB();
 app.use("/api", require("./routes/studentRoutes"));
 
 // server start
-app.listen(5000, () => {
-  console.log("Server running on port 5000 🚀");
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT} 🚀`);
 });
